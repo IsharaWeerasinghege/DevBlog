bdac91beaa32cb2e523de9f0d82816a6fc1abc6e
WSishara96 <surangaishara8@gmail.com>
2022-12-03 22:11:11 +0530

    version 1.1

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -11,7 +11,7 @@ const Categories = () => {
   }, []);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
       <h3 className="text-xl mb-6 font-semibold border-b pb-2">Categories</h3>
       {categories?.map((category) => (
         <Link href={`/category/${category.slug}`} key={category.name + Math.random()} className="block font-sans font-semibold items-center w-full pb-3 mb-1 hover:text-blue-900 transition-all duration-500">
